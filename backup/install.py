c5c4d5b2b7cad6609774dcb00f3f360dd233a5bc
HiDoYa <hiroyagojo@gmail.com>
2023-03-18 22:04:14 -0700

    Add additional backup script guidance

--- backup/install.py
+++ backup/install.py
@@ -14,6 +14,7 @@ with open(backup_script_fname, 'w') as f:
 
 # Template plist
 plist_fname = 'com.hiroyagojo.backup.plist'
+unique_label = 'com.hiroyagojo.backup.rclonebackups'
 scripts_dir = os.environ.get("SCRIPTS_DIR")
 backup_script_path = f"{scripts_dir}/backup/{backup_script_fname}"
 err_path = f"{scripts_dir}/backup/err.log"
@@ -21,6 +22,7 @@ log_path = f"{scripts_dir}/backup/out.log"
 template = env.get_template(f"{plist_fname}.j2")
 result = template.render(
     backup_script_path=backup_script_path,
+    unique_label=unique_label,
     err_path=err_path,
     log_path=log_path,
 )
@@ -36,6 +38,12 @@ shutil.copyfile(backup_script_fname, backup_dest_path)
 plist_dest_path = f"{home}/Library/LaunchAgents/{plist_fname}"
 shutil.copyfile(plist_fname, plist_dest_path)
 
+uid = os.environ.get("UID")
+
 print("Run the following:")
 print(f"chmod +x {backup_dest_path}")
 print(f"launchctl load {plist_dest_path}")
+
+print("\nDebugging commands:")
+print(f"launchctl print gui/{uid}/{unique_label}")
+print(f"launchctl list | grep {unique_label}")
